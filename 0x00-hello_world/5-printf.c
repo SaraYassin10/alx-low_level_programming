4f15e72d5542364be9469a94b1c14aefde6e1d06
sara <sara@gmail.com>
2023-09-07 10:59:33 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-/**
+/*
  * main - Entry point
  *
- * Description: A program prints a lie using prontf
+ * Description: A program prints a line with printf.
  *
- * Return- 0 (Success)
-*/ 
+ * Return: 0 (Success)
+*/
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n") 
-return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
