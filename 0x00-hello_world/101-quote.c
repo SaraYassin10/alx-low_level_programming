e2801c79076b1003cf4509f54c95dac16a45e3fa
sara <sara@gmail.com>
2023-09-07 12:53:12 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
  *
  * Description: print a quote using write function.
  *
- * Return: 1 (error).
+ * Return: 1 (success).
 */
 
 int main(void)
 {
 char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1.quo.5);
+write(1, quo, 5);
 return (1);
 }
