0e5b83dd9363b24d3e7273b6062c5bcc5537ad97
sara <sara@gmail.com>
2023-09-08 05:55:10 -0700

    i

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: A program
+ *
+ * Return : 0 (Success)
+*/
+
+int main(void)
+{
+int d = 0;
+
+while (d <= 9)
+{
+	printf("%i", d);
+	d++;
+}
+printf("\n");
+return (0);
+}
