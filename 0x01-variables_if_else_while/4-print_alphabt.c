0bf396a6c16ced2ec01e70315895c02e1db65967
sara <sara@gmail.com>
2023-09-08 05:45:11 -0700

    i

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,22 +3,24 @@
 /**
  * main- Entry point
  *
- * Description: A program prints alphabets in lowercase except q and e
+ * Description: A program prints alphabets in lowercase except q and e.
  *
  * Return: 0 (Success)
 */
 
 int main(void)
 {
-	char c = 'a';
-	
-	while (c <= 'z')
+char c = 'a';
+
+while (c <= 'z')
 {
-		if(c == 'q'|| c == 'e')
-		c++;
-	putchar(c);
+	if (c == 'q' || c == 'e')
 	c++;
+
+putchar(c);
+c++;
 }
-        putchar('\n');
-	return (0);
+putchar('\n');
+
+return (0);
 }
